314c949c61757b42ebb921510055fb66949fa514
Alex Seufert <amseufert@gmail.com>
2020-07-03 08:46:47 -0400

    Question 4 - Alex

--- assignment_1_group_1/Program.cs
+++ assignment_1_group_1/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace assignment_1_group_1
 {
     class Program
     {
+        static void question_four()
+        {
+            Console.WriteLine("Input string:");
+            string input = Console.ReadLine();
+            // iniitialize objects
+            Dictionary<char, int> data = new Dictionary<char, int>();
+            string output = "";
+
+            foreach (char c in input)
+            {
+                bool added = data.TryAdd(c, 1);
+                // increment on conflict
+                if (!added) {
+                    data[c] += 1;
+                }
+            }
+            foreach (KeyValuePair<char, int> pair in data.OrderByDescending(r => r.Value))
+            {
+                // creates new string for each char based on value count
+                // and appends to output string
+                output += new string(pair.Key, pair.Value); 
+            }
+            Console.WriteLine(output);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            question_four();
         }
     }
 }
