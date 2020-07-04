28b6f5629374666e52bf5eaf0b6aa3aed215f11d
Alex Seufert <amseufert@gmail.com>
2020-07-03 20:36:06 -0400

    question 2 alex

--- assignment_1_group_1/Program.cs
+++ assignment_1_group_1/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace assignment_1_group_1
 {
     class Program
     {
+        static string StringReverse(string s)
+        {
+            LinkedList<string> final = new LinkedList<string>();
+            string currentWord = "";
+            // single for loop is O(n)
+            for (var i = s.Length; i > 0; i--)
+            {
+                char value = s[i - 1];
+                if (value == ' ')
+                {
+                    // linked list AddFirst is O(1)
+                    final.AddFirst(currentWord);
+                    currentWord = "";
+                } else
+                {
+                    // string appending is O(1)
+                    currentWord += value;
+                }
+            }
+            final.AddFirst(currentWord);
+
+            return string.Join(" ", final);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string msg = StringReverse("University of South Florida");
+            Console.WriteLine(msg);
         }
     }
 }
