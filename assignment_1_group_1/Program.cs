49d6e1bcedead14adf4caf1a6bd78d5b0d8757ba
Deborah Gause <dgause@usf.edu>
2020-07-05 12:07:25 -0400

    Q2

--- assignment_1_group_1/Program.cs
+++ assignment_1_group_1/Program.cs
@@ -4,9 +4,28 @@ namespace assignment_1_group_1
 {
     class Program
     {
+        public static string StringReverse(string s)
+        {
+            string result = "";
+            string temp = "";
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (s[i] == ' ')
+                {
+                    result = temp + " " + result;
+                    temp = "";
+                }
+                else
+                    temp += s[i];
+            }
+            result = temp + " " + result;
+
+            Console.WriteLine(result);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string output = StringReverse("University of South Florida");
+            Console.WriteLine(output);
         }
     }
 }
